ba9623a90a9de8680d311a26d9c4b7fab2ec4ba1
bryce batten <Bryce-Batten@live.nmit.ac.nz>
2021-03-24 11:21:31 +1300

    Commiting week 4

---
+++ src/index.js
@@ -0,0 +1,46 @@
+let country = 'new zealand'
+let city = 'nelsen'
+let postcode = 7010 
+consele.log(country)
+console.log(city)
+console.log(postcode) 
+
+let isISland = false
+let language
+
+console.log (typeof isISland)
+console.log (typeof language)
+ 
+
+
+language = {
+    name: 'Ali',
+    city: 'Mosul',
+    country : 'iraq'
+}
+console.log(language.country)
+
+ 
+
+
+console.log(typeof language)
+console.log(language.city = console.log)
+
+
+
+console.log(postcode)
+postcode++
+postcode--
+console.log(postcode)
+
+if (postcode === 7010) {console.log('nelson')}{console.log('not nelson')}
+
+
+
+
+const x = {
+    city: 'nelson'
+}
+const y = {
+    city: 'nelson'
+}
