cf8da0aba45330ad0bb42a7905b77fad9e693475
bryce batten <Bryce-Batten@live.nmit.ac.nz>
2021-03-24 12:21:28 +1300

    Week 5

---
+++ src/weekfive.js
@@ -0,0 +1,29 @@
+let grandstudent = 90
+
+switch (grandstudent) {
+    case (grandstudent):
+        console.log('my grade is A+')
+        break
+        case (gradstudent >=80):
+            console.log('my grade is 8+')
+            braek
+}
+
+let person = {
+name: 'tony',
+age: 20,
+driver: null    
+};
+
+let isstudent = true
+
+let price = isstudent ? 8 : 12
+console.log(price); // 8
+
+//let isstudent = false;
+let issenior = true;
+price = isstudent ? 8 : issenior ? 6 : 10
+console.log(price);// 6
+
+let isstudent = true;
+let price = 12;isstudent
